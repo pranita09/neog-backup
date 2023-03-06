a4253f1a94dc84504897435fce03c0d7c72e7fa5
Pranita <pfulsundar8@gmail.com>
2023-03-06 15:20:16 +0530

    Initial commit

---
+++ FP-Practice-Set-3/sol10.js
@@ -0,0 +1,16 @@
+// Question
+// Write an ES6 function that takes an array of objects representing books with properties title, author and pageCount. Return the all book titles that have more than 700 pages.
+
+const books = [
+    { title: "The Lord of the Rings", author: "J.R.R. Tolkien", pageCount: 1178 },
+    { title: "To Kill a Mockingbird", author: "Harper Lee", pageCount: 281 },
+    { title: "The Hitchhiker's Guide to the Galaxy", author: "Douglas Adams", pageCount: 193 },
+    { title: "The Name of the Wind", author: "Patrick Rothfuss", pageCount: 662 },
+];
+  
+// Your code here
+const getTitlesWithMoreThan500Pages = arrayOfBooks => arrayOfBooks.filter(({pageCount}) => pageCount > 700).map(({title}) => title);
+  
+const booksWithMoreThan500Pages = getTitlesWithMoreThan500Pages(books);
+console.log(booksWithMoreThan500Pages); 
+// Output: ["The Lord of the Rings"]
---
+++ FP-Practice-Set-3/sol9.js
@@ -0,0 +1,16 @@
+// Question
+// Write an ES6 function that takes an array of objects containing car information (make, model, year) and returns an array with only the car model that were made after the year 2012.
+
+const cars = [
+    { make: "Toyota", model: "Corolla", year: 2010 },
+    { make: "Honda", model: "Civic", year: 2012 },
+    { make: "Toyota", model: "Camry", year: 2015 },
+    { make: "Ford", model: "Mustang", year: 2018 },
+];
+  
+// Your code here
+const getCarModel = arrayOfCars => arrayOfCars.filter(({year}) => year > 2012).map(({model}) => model);
+  
+const carModels = getCarModel(cars);
+console.log(carModels);
+// Output: ["Camry", "Mustang"]
