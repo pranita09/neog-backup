24407ea38706704bc9bc8b6b82fddc85e15c48e4
Pranita <pfulsundar8@gmail.com>
2023-03-28 01:12:21 +0530

    refactor: added filter in component

--- ReactJS-Practice-Set-1/practice-set-1/src/ProductData.jsx
+++ ReactJS-Practice-Set-1/practice-set-1/src/ProductData.jsx
@@ -1,25 +1,42 @@
-
-const ProductCard = ({id, name}) =>{
-    return(
-        <>
-          <li key={id}>{name}</li>  
-        </>
-    )
-}
-
-const ProductData = ({productList}) => {
-    return(
-        <>
-            <div style={{border: '1px solid black', borderRadius:'5px', margin: '10px auto',padding: '1rem', width: '40%', }}>
-                <h1>Product Names</h1>
-                <ol>
-                    {productList.map(({name},index)=>(
-                        <ProductCard id={index} name={name}/>
-                    ))}
-                </ol>
-            </div>
-        </>
-    )
-}
+const ProductCard = ({ id, name, quantity, sales }) => {
+    return (
+      <>
+        <li key={id}>
+          Name: {name}, Quantity: {quantity}, Sales: {sales}
+        </li>
+      </>
+    );
+};
+  
+const ProductData = ({ productList }) => {
+    return (
+      <div>
+        <div
+          style={{
+            border: "1px solid black",
+            borderRadius: "5px",
+            margin: "10px auto",
+            padding: "1rem",
+            width: "60%"
+          }}
+        >
+          <h1>Product Names</h1>
+          <ul>
+            {productList
+              .filter(({ quantity, sales }) => sales > quantity)
+              .map(({ name, quantity, sales }, index) => (
+                <ProductCard
+                  id={index}
+                  name={name}
+                  quantity={quantity}
+                  sales={sales}
+                />
+              ))}
+          </ul>
+        </div>
+      </div>
+    );
+};
 
 export default ProductData;
+  
