0f64ece71bf62339216a36e181b34dca151316eb
Pranita <pfulsundar8@gmail.com>
2023-03-27 19:42:31 +0530

    refactor: styling of EmployeeCard

--- ReactJS-Practice-Set-1/practice-set-1/src/EmployeeCard.jsx
+++ ReactJS-Practice-Set-1/practice-set-1/src/EmployeeCard.jsx
@@ -1,10 +1,10 @@
 
 const EmployeeCard = ({name, designation, workExperience}) => {
     return(
-        <div style={{border: '1px solid black', borderRadius:'5px', margin: '10px auto',padding: '1rem', width: '50%', }}>
+        <div style={{border: '1px solid black', borderRadius:'5px', margin: '10px auto',padding: '1rem', width: '40%', }}>
             <h1>{name}</h1>
-            <p>Designation: {designation}</p>
-            <p>Work Experience: {workExperience}</p>
+            <p><span style={{color: "green"}}>Designation:</span> {designation}</p>
+            <p><span style={{color: "blue"}}>Work Experience:</span> {workExperience}</p>
         </div>
     );
 }
