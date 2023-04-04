aa96c8f57ab4344493774d4d13e6b4ca77b86342
Pranita <pfulsundar8@gmail.com>
2023-04-04 21:21:44 +0530

    import and render components

--- ReactJS-Practice-Set-3/practice-set-3/src/App.js
+++ ReactJS-Practice-Set-3/practice-set-3/src/App.js
@@ -5,10 +5,20 @@ import DisplayCartoons from './set-3/QuestionTwo';
 import { cartoons } from './set-3/Data';
 import { characters, itemList, todoItems, todoItems2, books } from './set-4/Data';
 import ShowCharacters from './set-4/QuestionOne';
+import FontSizeHandler from './set-4/QuestionTwo';
+import ListingItems from './set-4/QuestionThree';
+import ToDoList from './set-4/QuestionFour';
+import TodoList from './set-4/QuestionFive';
+import BooksByGenre from './set-4/QuestionSix';
 
 function App() {
   return (
     <div className="App">
+    <BooksByGenre booksData={books} />
+    <TodoList todoItemsData={todoItems2}/>
+    <ToDoList toDoItemsData={todoItems} />
+    <ListingItems itemsData={itemList} />
+    <FontSizeHandler />
     <ShowCharacters charactersData={characters}/>
     {/* <DisplayCartoons cartoonsData={cartoons}/> */}
     {/* <DisplayImage imgLink={'https://picsum.photos/200'} imgWidth={'70%'} imgHeight={'70%'} /> */}
