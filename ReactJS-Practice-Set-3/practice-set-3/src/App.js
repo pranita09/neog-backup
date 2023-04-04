6118be2afc8ee714967fd9aa046050a0d65f2b0f
Pranita <pfulsundar8@gmail.com>
2023-04-04 21:17:40 +0530

    all data imported from set4/data.js

--- ReactJS-Practice-Set-3/practice-set-3/src/App.js
+++ ReactJS-Practice-Set-3/practice-set-3/src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 import DisplayImage from './set-3/QuestionOne';
 import DisplayCartoons from './set-3/QuestionTwo';
 import { cartoons } from './set-3/Data';
+import { characters, itemList, todoItems, todoItems2, books } from './set-4/Data';
+import ShowCharacters from './set-4/QuestionOne';
 
 function App() {
   return (
     <div className="App">
+    <ShowCharacters charactersData={characters}/>
     {/* <DisplayCartoons cartoonsData={cartoons}/> */}
     {/* <DisplayImage imgLink={'https://picsum.photos/200'} imgWidth={'70%'} imgHeight={'70%'} /> */}
     </div>
