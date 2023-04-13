ea55a7def78a1129c40e47a59453702158272056
Pranita <pfulsundar8@gmail.com>
2023-04-13 20:08:43 +0530

    react-roter-dom dependancy install

--- React-PS-8/e-commerce/src/App.jsx
+++ React-PS-8/e-commerce/src/App.jsx
@@ -1,12 +1,18 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import {Routes, Route} from 'react-router-dom';
 import './App.css'
+import Home from './components/Home';
+
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      
+    <Routes>
+      <Route path='/home' element={<Home />} />
+    </Routes>
+      <h1>E commerce</h1>
     </div>
   )
 }
